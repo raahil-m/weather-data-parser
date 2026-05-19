7e1623ad39a6eedd692185074014342fe574f624
raahil-m <95246408+raahil-m@users.noreply.github.com>
2026-05-19 09:37:50 -0400

    Add styling constants and change_color function
    Added color constants and change_color function for styling.

--- weather.py
+++ weather.py
@@ -4,7 +4,29 @@ import sys
 from configparser import ConfigParser
 from urllib import error, parse, request
 
-import style
+# =========================
+# Styling / Colors
+# =========================
+
+PADDING = 20
+
+RED = "\033[1;31m"
+BLUE = "\033[1;34m"
+CYAN = "\033[1;36m"
+GREEN = "\033[0;32m"
+YELLOW = "\033[33m"
+WHITE = "\033[37m"
+
+REVERSE = "\033[;7m"
+RESET = "\033[0m"
+
+
+def change_color(color):
+    print(color, end="")
+
+# =========================
+# Weather Config
+# =========================
 
 BASE_WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
 
